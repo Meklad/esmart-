35952aa6c11190a5e4a39c7450b8a6c982b88e58
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2022-09-20 10:33:35 +0200

    Modify phpnuit.xml and update github workflow

--- tests/Feature/CampaginDatabaseTest.php
+++ tests/Feature/CampaginDatabaseTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Http\Resources\Apis\CampaignResource;
-use App\Models\Campaign;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
