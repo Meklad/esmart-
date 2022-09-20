907cc748fdffb5884f47769151eeaf5932499dd3
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2022-09-20 10:42:36 +0200

    update workflow

--- tests/Feature/CampaginDatabaseTest.php
+++ tests/Feature/CampaginDatabaseTest.php
@@ -4,13 +4,10 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class CampaginDatabaseTest extends TestCase
 {
-    use DatabaseMigrations;
-
     public function setUp(): void
     {
         parent::setUp();
